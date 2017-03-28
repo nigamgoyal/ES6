41e1bea7ba844d1e3cab75cbd1efe6c79bfc9dbf
Nigam Goyal <nigam@concret.io>
2017-03-28 23:25:22 +0530

    let keyword

--- app.js
+++ app.js
@@ -12,6 +12,36 @@ var arrowGreeting = (message,name) => message + name;
 var simpleArrowGreeting = name => "Hello "+name;
 
 
+// The Let Keyword in ES6 //
+
+// in Vanila Javascript
+var message = "hi";
+
+{
+    var message = "bye";    
+}
+
+console.log(message);
+// In ES6
+
+let messageES = 'hi';
+
+{
+    let messageES = 'bye';
+}
+
+console.log(messageES);
+
+var fs = [];
+
+for(let i = 0; i < 10;i++){
+    fs.push(function(){
+        console.log(i);
+    });
+}
 
 
+fs.forEach(function(f){
+    f();
+});
 
