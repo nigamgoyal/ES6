06332b08faccd3c896f0dd1acf185be456d64c85
Nigam Goyal <nigam@concret.io>
2017-03-28 23:16:29 +0530

    Arrow Function

---
+++ app.js
@@ -0,0 +1,17 @@
+var createGreeting = function(message,name){
+    return message + name;
+}
+
+// Arrow function
+var arrowfunction = (message,name) => {
+    return message + name;
+}
+
+var arrowGreeting = (message,name) => message + name;
+
+var simpleArrowGreeting = name => "Hello "+name;
+
+
+
+
+
